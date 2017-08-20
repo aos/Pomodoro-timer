649b29928bb9b3051f5db08d7f20bbeb64886870
Aos <aosdab@gmail.com>
2017-08-20 18:06:49 -0400

    Wrapped script in closure to remove global namespace pollution

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -12,163 +12,169 @@ Features:
 -- Must be able to pause timer
 -- Must be able to reset timer 
 
-Copyright (c) 2017 Aos Dabbagh (aosdab@gmail.com)
+Created by Aos (@aosdabbagh)
+
+License: MIT
 */
 
-var timerID,
+(function() {
+
+  var timerID,
     duration,
     isPause,
     switchTimer = false;
 
-/* 
+  /* 
   Handlers 
-*/
-// Main Timer
-var display = document.querySelector("#mainTimer");
-var startTimerBtn = document.querySelector("#startTimerBtn");
-var stopTimerBtn = document.querySelector("#stopTimerBtn");
-var resetBtn = document.querySelector("#reset");
-stopTimerBtn.hidden = true;
+  */
+  // Main Timer
+  var display = document.querySelector("#mainTimer");
+  var startTimerBtn = document.querySelector("#startTimerBtn");
+  var stopTimerBtn = document.querySelector("#stopTimerBtn");
+  var resetBtn = document.querySelector("#reset");
+  stopTimerBtn.hidden = true;
 
-var currentSesh = document.querySelector("#currentSesh");
-var currentBreak = document.querySelector("#currentBreak");
-var audio = document.querySelector("audio");
+  var currentSesh = document.querySelector("#currentSesh");
+  var currentBreak = document.querySelector("#currentBreak");
+  var audio = document.querySelector("audio");
 
-// Session Timer
-var seshDisplay = document.querySelector("#seshTimer");
-var seshNegBtn = document.querySelector("#seshNeg");
-var seshPosBtn = document.querySelector("#seshPos");
+  // Session Timer
+  var seshDisplay = document.querySelector("#seshTimer");
+  var seshNegBtn = document.querySelector("#seshNeg");
+  var seshPosBtn = document.querySelector("#seshPos");
 
-// Break Timer
-var breakDisplay = document.querySelector("#breakTimer");
-var breakNegBtn = document.querySelector("#breakNeg");
-var breakPosBtn = document.querySelector("#breakPos");
+  // Break Timer
+  var breakDisplay = document.querySelector("#breakTimer");
+  var breakNegBtn = document.querySelector("#breakNeg");
+  var breakPosBtn = document.querySelector("#breakPos");
 
-/* 
+  /* 
   Event listeners
-*/
-// Main timer
-startTimerBtn.addEventListener("click", eventsOnClickStart);
-stopTimerBtn.addEventListener("click", eventsOnClickStop);
-resetBtn.addEventListener("click", resetTimer);
-// Session timer
-seshNegBtn.addEventListener("click", seshTimerNeg);
-seshPosBtn.addEventListener("click", seshTimerPos);
-// Break timer
-breakPosBtn.addEventListener("click", breakTimerLong);
-breakNegBtn.addEventListener("click", breakTimerShort);
+  */
+  // Main timer
+  startTimerBtn.addEventListener("click", eventsOnClickStart);
+  stopTimerBtn.addEventListener("click", eventsOnClickStop);
+  resetBtn.addEventListener("click", resetTimer);
+  // Session timer
+  seshNegBtn.addEventListener("click", seshTimerNeg);
+  seshPosBtn.addEventListener("click", seshTimerPos);
+  // Break timer
+  breakPosBtn.addEventListener("click", breakTimerLong);
+  breakNegBtn.addEventListener("click", breakTimerShort);
 
-/*
+  /*
   Controllers
-*/
-// Main timer
-function eventsOnClickStart(e) {
-  startTimer();
-  // Hides start button and shows pause button on click
-  e.currentTarget.hidden = true;
-  stopTimerBtn.hidden = false;
-  // Disables timer control buttons when timer is running
-  resetBtn.disabled = true;
-  // Handles pause functionality
-  isPause = false;
-};
-function eventsOnClickStop(e) {
-  clearInterval(timerID);
-  // Hides pause button and shows start button on click
-  e.currentTarget.hidden = true;
-  startTimerBtn.hidden = false;
-  // Enables timer control buttons when timer is paused
-  resetBtn.disabled = false;
-  // Handles pause functionality
-  isPause = true;
-};
-function resetTimer() {
-  var timer = Number(seshDisplay.textContent);
-  display.textContent = (timer < 10 ? "0" + timer : timer) + ":00";
-  duration = timer * 60;
-  currentSesh.classList.remove("current");
-  currentBreak.classList.remove("current");
-  display.classList.remove("current");
-  isPause = undefined;
-  switchTimer = false;
-}
-
-// Session timer
-function seshTimerPos() {
-  seshDisplay.textContent = Number(seshDisplay.textContent) + 1;
-  if (isPause == undefined) {
+  */
+  // Main timer
+  function eventsOnClickStart(e) {
+    startTimer();
+    // Hides start button and shows pause button on click
+    e.currentTarget.hidden = true;
+    stopTimerBtn.hidden = false;
+    // Disables timer control buttons when timer is running
+    resetBtn.disabled = true;
+    // Handles pause functionality
+    isPause = false;
+  };
+  function eventsOnClickStop(e) {
+    clearInterval(timerID);
+    // Hides pause button and shows start button on click
+    e.currentTarget.hidden = true;
+    startTimerBtn.hidden = false;
+    // Enables timer control buttons when timer is paused
+    resetBtn.disabled = false;
+    // Handles pause functionality
+    isPause = true;
+  };
+  function resetTimer() {
     var timer = Number(seshDisplay.textContent);
     display.textContent = (timer < 10 ? "0" + timer : timer) + ":00";
-  }
-};
-function seshTimerNeg() {
-  seshDisplay.textContent = Number(seshDisplay.textContent) - 1;
-  if (seshDisplay.textContent < 1) {
-    seshDisplay.textContent = 1;
-  }
-  if (isPause == undefined) {
-    var timer = Number(seshDisplay.textContent);
-    display.textContent = (timer < 10 ? "0" + timer : timer) + ":00";
-  }
-};
-
-// Break timer
-function breakTimerLong() {
-  breakDisplay.textContent = 10;
-  breakNegBtn.classList.remove("btn-options-selected");
-  breakPosBtn.classList.remove("btn-options-selected");
-  breakPosBtn.classList.add("btn-options-selected");
-};
-function breakTimerShort() {
-  breakDisplay.textContent = 5;
-  breakNegBtn.classList.remove("btn-options-selected");
-  breakPosBtn.classList.remove("btn-options-selected");
-  breakNegBtn.classList.add("btn-options-selected");
-};
-
-/* 
-  Timer functionality 
-*/
-function startTimer() {
-  // Handles pause timer, and duration of session and break timer
-  if (isPause) {
-    // Do nothing (aka don't reset duration)
-  } else if (switchTimer) {
-    duration = Number(breakDisplay.textContent) * 60;
+    duration = timer * 60;
     currentSesh.classList.remove("current");
-    currentBreak.classList.add("current");
-    display.classList.add("current");
-  } else {
-    duration = Number(seshDisplay.textContent) * 60;
-    currentSesh.classList.add("current");
     currentBreak.classList.remove("current");
     display.classList.remove("current");
+    isPause = undefined;
+    switchTimer = false;
   }
 
-  timerID = setInterval(function () {
-    var minutes, seconds;
-    // Get integer value
-    minutes = parseInt(duration / 60, 10);
-    seconds = parseInt(duration % 60, 10);
-
-    // Pad with leading "0" if less than 10
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    // Display timer
-    display.textContent = minutes + ":" + seconds;
-
-    // Decrement by 1 second
-    duration--;
-
-    if (duration < 0) {
-      // Stop timer first
-      clearInterval(timerID);
-      // Change logic to switch to break timer
-      switchTimer = !switchTimer;
-      // Restart timer
-      audio.play();
-      startTimer();
+  // Session timer
+  function seshTimerPos() {
+    seshDisplay.textContent = Number(seshDisplay.textContent) + 1;
+    if (isPause == undefined) {
+      var timer = Number(seshDisplay.textContent);
+      display.textContent = (timer < 10 ? "0" + timer : timer) + ":00";
     }
-  }, 1000)
-}
+  };
+  function seshTimerNeg() {
+    seshDisplay.textContent = Number(seshDisplay.textContent) - 1;
+    if (seshDisplay.textContent < 1) {
+      seshDisplay.textContent = 1;
+    }
+    if (isPause == undefined) {
+      var timer = Number(seshDisplay.textContent);
+      display.textContent = (timer < 10 ? "0" + timer : timer) + ":00";
+    }
+  };
+
+  // Break timer
+  function breakTimerLong() {
+    breakDisplay.textContent = 10;
+    breakNegBtn.classList.remove("btn-options-selected");
+    breakPosBtn.classList.remove("btn-options-selected");
+    breakPosBtn.classList.add("btn-options-selected");
+  };
+  function breakTimerShort() {
+    breakDisplay.textContent = 5;
+    breakNegBtn.classList.remove("btn-options-selected");
+    breakPosBtn.classList.remove("btn-options-selected");
+    breakNegBtn.classList.add("btn-options-selected");
+  };
+
+  /* 
+  Timer functionality 
+  */
+  function startTimer() {
+    // Handles pause timer, and duration of session and break timer
+    if (isPause) {
+      // Do nothing (aka don't reset duration)
+    } else if (switchTimer) {
+      duration = Number(breakDisplay.textContent) * 60;
+      currentSesh.classList.remove("current");
+      currentBreak.classList.add("current");
+      display.classList.add("current");
+    } else {
+      duration = Number(seshDisplay.textContent) * 60;
+      currentSesh.classList.add("current");
+      currentBreak.classList.remove("current");
+      display.classList.remove("current");
+    }
+
+    timerID = setInterval(function () {
+      var minutes, seconds;
+      // Get integer value
+      minutes = parseInt(duration / 60, 10);
+      seconds = parseInt(duration % 60, 10);
+
+      // Pad with leading "0" if less than 10
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      // Display timer
+      display.textContent = minutes + ":" + seconds;
+
+      // Decrement by 1 second
+      duration--;
+
+      if (duration < 0) {
+        // Stop timer first
+        clearInterval(timerID);
+        // Change logic to switch to break timer
+        switchTimer = !switchTimer;
+        // Restart timer
+        audio.play();
+        startTimer();
+      }
+    }, 1000)
+  }
+
+})();
